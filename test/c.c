d0de4fcb578a3be6afd089a67150a19d0a31961a
upsetgrass <18328591636@163.com>
2025-08-11 17:57:47 +0800

    new

--- test/c.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("%zu\n", sizeof(long));
-	return 0;
-}
--- test/d.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<stdint.h>
-int main()
-{
-	char c = 0xff;
-	printf(c == 0xff ? "T\n" : "F\n");
-}
